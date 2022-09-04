73d8444a37c222d01729b62ff7fa5aa58c573fd2
Jonathan Grim <jonjongrim@gmail.com>
2022-09-04 12:21:54 -0400

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Jonathan Grim on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
